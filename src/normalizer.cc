0f866ee891e12c547f89be31479b87e8ea3558cf
Marcin Junczys-Dowmunt <marcinjd@microsoft.com>
2021-05-07 09:13:31 -0700

    minimize diff

--- src/normalizer.cc
+++ src/normalizer.cc
@@ -48,7 +48,6 @@ Normalizer::~Normalizer() {}
 
 void Normalizer::Init() {
   absl::string_view index = spec_->precompiled_charsmap();
-
   if (index.empty()) {
     LOG(INFO) << "precompiled_charsmap is empty. use identity normalization.";
   } else {
@@ -76,7 +75,6 @@ void Normalizer::Init() {
 util::Status Normalizer::Normalize(absl::string_view input,
                                    std::string *normalized,
                                    std::vector<size_t> *norm_to_orig) const {
-
   norm_to_orig->clear();
   normalized->clear();
 
@@ -131,56 +129,49 @@ util::Status Normalizer::Normalize(absl::string_view input,
   // With this prefix, "world" and "hello world" are converted into
   // "_world" and "_hello_world", which help the trainer to extract
   // "_world" as one symbol.
-  if (!treat_whitespace_as_suffix_ && spec_->add_dummy_prefix()) 
-    add_ws();
+  if (!treat_whitespace_as_suffix_ && spec_->add_dummy_prefix()) add_ws();
 
   std::unique_ptr<CaseEncoder> case_encoder = CaseEncoder::Create(spec_->encode_case(), spec_->decode_case());
-  
   case_encoder->setNormalizer(
-    [this](absl::string_view input) { 
-      return NormalizePrefix(input); 
+    [this](absl::string_view input) {
+      return NormalizePrefix(input);
     }
   );
 
-  ////////////////////////////////////////////////////////////////////////////////////
-
   bool is_prev_space = spec_->remove_extra_whitespaces();
   while (!input.empty()) {
-
     auto p = case_encoder->normalizePrefix(input);
     auto sp = p.first;
 
     // Removes heading spaces in sentence piece,
     // if the previous sentence piece ends with whitespace.
-    while (is_prev_space && absl::ConsumePrefix(&sp, " ")) {}
+    while (is_prev_space && absl::ConsumePrefix(&sp, " ")) {
+    }
 
     if (!sp.empty()) {
+      const char *data = sp.data();
       for (size_t n = 0; n < sp.size(); ++n) {
-        if (spec_->escape_whitespaces() && sp.data()[n] == ' ') {
+        if (spec_->escape_whitespaces() && data[n] == ' ') {
           // replace ' ' with kSpaceSymbol.
           normalized->append(kSpaceSymbol.data(), kSpaceSymbol.size());
           for (size_t m = 0; m < kSpaceSymbol.size(); ++m) {
             norm_to_orig->push_back(consumed);
           }
         } else {
-          normalized->append(sp.data() + n, 1);
+          *normalized += data[n];
           norm_to_orig->push_back(consumed); 
         }
       }
-
       // Checks whether the last character of sp is whitespace.
       is_prev_space = absl::EndsWith(sp, " ");
     }
 
+    consumed += p.second; 
+    input.remove_prefix(p.second);
     if (!spec_->remove_extra_whitespaces()) {
       is_prev_space = false;
     }
-    
-    consumed += p.second; 
-    input.remove_prefix(p.second);
   }
-
-  ////////////////////////////////////////////////////////////////////////////////////
 
   // Ignores tailing space.
   if (spec_->remove_extra_whitespaces()) {
